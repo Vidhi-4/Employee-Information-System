eb857dba6f3876362edbc7989376bf551479bfcb
Vidhi-4 <vidhi.bajpai.cs19@ggits.net>
2022-02-26 15:39:11 +0530

    This is an employee Information System file.

---
+++ src/employee/conn.java
@@ -0,0 +1,19 @@
+package employee;
+
+import java.sql.*;
+
+public class conn{
+
+    public Connection c;
+    public Statement s;
+
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///project","root","");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
